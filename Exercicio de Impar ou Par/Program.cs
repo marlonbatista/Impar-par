82246efdc8cb4ece7422e48fdecb00efa290aa1c
Marlon Batista <marlon.batista18@hotmail.com>
2019-01-30 14:47:24 -0200

    commit

---
+++ Exercicio de Impar ou Par/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_de_Impar_ou_Par
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//Declaração de variável
+            double num;
+            //Entrada de Dados
+
+            Console.Write("Informe o valor :");
+            num = double.Parse(Console.ReadLine());
+            //processamento
+
+            if ( num%2==0)
+            {
+                Console.Write("\nO número é par : " + num);
+            }
+            else
+            {
+                Console.Write("\nO número é impar :" + num);
+            }
+            Console.ReadKey();
+        }
+    }
+}
